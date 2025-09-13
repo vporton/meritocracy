0887ed1d6a4c97923da14f2218b45c4fbf512b82
Victor Porton <porton.victor@gmail.com>
2025-09-13 21:40:41 +0300

    fix bug

--- backend/src/runners/UtilityRunners.ts
+++ backend/src/runners/UtilityRunners.ts
@@ -498,9 +498,6 @@ export abstract class BaseRunner implements TaskRunner {
       
       // Log the response to the database
       await this.logOpenAIResponse(customId, response, undefined);
-      
-      // FIXME: not called for non-OpenAI runners.
-      this.onOutput(customId, content);
 
       return content;
     } catch (error) {

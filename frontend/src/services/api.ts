4f10403f1ea693800e4085498eccf17350168f99
Victor Porton <porton.victor@gmail.com>
2025-09-14 12:32:00 +0300

    fix bug

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,6 +9,15 @@ const api = axios.create({
   },
 })
 
+// Add request interceptor to include auth token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
 interface User {
   id: number;
   email: string;

a62b5d3cbe8dab0e5ca8f2f5dee65613cf54bb89
Victor Porton <porton.victor@gmail.com>
2025-09-16 05:45:14 +0300

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +11,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },

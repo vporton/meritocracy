dd5845aebcac7c1e91e448e23df40c253685a788
Victor Porton <porton.victor@gmail.com>
2025-09-26 17:29:22 +0300

    fix DB migrations

--- backend/prisma/migrations/20250926150931_add_kyc_fields/migration.sql
+++ backend/prisma/migrations/20250926150931_add_kyc_fields/migration.sql
@@ -1,9 +1,1 @@
--- CreateIndex
-CREATE INDEX "users_kycStatus_idx" ON "users"("kycStatus");
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "kycRejectedAt" DATETIME;
-ALTER TABLE "users" ADD COLUMN "kycRejectionReason" TEXT;
-ALTER TABLE "users" ADD COLUMN "kycSessionId" TEXT;
-ALTER TABLE "users" ADD COLUMN "kycStatus" TEXT;
-ALTER TABLE "users" ADD COLUMN "kycVerifiedAt" DATETIME;
+-- This migration is now empty as all KYC fields were already added in the previous migration

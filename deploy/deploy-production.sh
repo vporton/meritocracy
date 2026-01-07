bb2fb9e63d9cf7be63aad97284e09e1735b32359
Victor Porton <porton.victor@gmail.com>
2026-01-07 16:35:55 +0200

    production deployment

---
+++ deploy/deploy-production.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# TODO@P2: VITE_WALLETCONNECT_PROJECT_ID
+flyctl deploy -c deploy/fly.production.toml \
+    --build-arg VITE_FRONTEND_URL=https://meritocracy.fly.dev \
+    --build-arg VITE_API_URL=https://meritocracy.fly.dev:445 \
+    --build-arg VITE_WALLETCONNECT_PROJECT_ID=your-walletconnect-project-id \
+    --build-arg VITE_GITHUB_CLIENT_ID=Iv23livDuDi7dm9P5vli \
+    --build-arg VITE_ORCID_CLIENT_ID=APP-CNFU262DB2VL0XQ3 \
+    --build-arg VITE_BITBUCKET_CLIENT_ID=PFdcCeTaGXXY723bfBuTe3deVJDstaf2 \
+    --build-arg VITE_GITLAB_CLIENT_ID=8a8b4b40b8d3d78ccde78b4c80ffd69db99ca6f022a42ac0159ab21c8d148da6 \
+    --build-arg VITE_ORCID_DOMAIN=orcid.org

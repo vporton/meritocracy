13359740a9584fb03908de70a482b8322919302e
Victor Porton <porton.victor@gmail.com>
2026-01-12 05:44:53 +0200

    fix WalletConnect ID

--- deploy/deploy-production.sh
+++ deploy/deploy-production.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# TODO@P2: VITE_WALLETCONNECT_PROJECT_ID
 flyctl deploy -c deploy/fly.production.toml \
     --build-arg VITE_FRONTEND_URL=https://merit.science-dao.org \
     --build-arg VITE_API_URL=https://merit.science-dao.org:445 \
-    --build-arg VITE_WALLETCONNECT_PROJECT_ID=your-walletconnect-project-id \
+    --build-arg VITE_WALLETCONNECT_PROJECT_ID=1b73cc17532df865d3f9377b3463fc42 \
     --build-arg VITE_GITHUB_CLIENT_ID=Iv23liACVkYe3qylnSpT \
     --build-arg VITE_ORCID_CLIENT_ID=APP-CNFU262DB2VL0XQ3 \
     --build-arg VITE_BITBUCKET_CLIENT_ID=PFdcCeTaGXXY723bfBuTe3deVJDstaf2 \
